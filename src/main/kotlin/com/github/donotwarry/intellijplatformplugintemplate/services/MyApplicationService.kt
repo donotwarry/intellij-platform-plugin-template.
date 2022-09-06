aeb7e51af89ef0f478a863a7c7fda7f805b50b76
GitHub Action <action@github.com>
2022-09-06 03:14:58 +0000

    Template cleanup

--- src/main/kotlin/com/github/donotwarry/intellijplatformplugintemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/donotwarry/intellijplatformplugintemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.donotwarry.intellijplatformplugintemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.donotwarry.intellijplatformplugintemplate.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/donotwarry/intellijplatformplugintemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/donotwarry/intellijplatformplugintemplate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.donotwarry.intellijplatformplugintemplate.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.donotwarry.intellijplatformplugintemplate.MyBundle
 
 class MyProjectService(project: Project) {
 
